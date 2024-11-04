811164acbe606ac7e5299dea91f8288e596e85bf
tsawke@desktop <12413104@mail.sustech.edu.cn>
2024-11-05 01:54:05 +0800

    Generally update by tsawke@desktop.

---
+++ src/HelloWorld.java
@@ -0,0 +1,22 @@
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class HelloWorld {
+    private static void createAndShowWindow() {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label = new JLabel("Hello World!");
+        frame.getContentPane().add(label);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run(){
+                createAndShowWindow();
+            }
+        });
+    }
+}
---
+++ update.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Generally update by tsawke@desktop."
+git push -u origin master
