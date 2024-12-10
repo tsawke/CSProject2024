07691e89e7e4ea704fc547d1e2655b6be4c1eb70
Monkey@DELL <monkey-hyx@qq.com>
2024-12-10 20:50:36 +0800

    Generally update by tsawke@desktop.

--- csproject/src/main/java/com/csproject/Archive.java
+++ csproject/src/main/java/com/csproject/Archive.java
@@ -6,18 +6,19 @@ import java.util.Properties;
 import java.util.stream.IntStream;
 
 public class Archive {
+    private static int idx = 0;
     public static void CreateDefaultArchiveByID(int id) throws Exception {
         Properties properties = new Properties();
         OutputStream out = new FileOutputStream("./csproject/target/archives/" + id + ".properties");
         // properties.setProperty("playerCnt", Integer.toString(playerCnt));
-        
         GameSystem.maps.stream()
             .forEach(m -> {
+                idx = 0;
                 IntStream.range(1, m.getHeight() + 1).forEach(
                     i -> {
                         IntStream.range(1, m.getWidth() + 1).forEach(
                             j -> {
-                                
+                                properties.setProperty(String.format("map2D_%d.%d.%d", ++idx, i, j), m.GetMapByIndex(i, j).toString);
                             }
                         );
                     }
